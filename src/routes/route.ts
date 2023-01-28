553b9ed4df5667948970b0bbf49ccfa474ebf896
sgitwhyd <sigitwahyu.mutu@gmail.com>
2023-01-28 11:37:16 +0700

    remove route folder

--- src/routes/route.ts
+++
@@ -1,9 +0,0 @@
-import Home from '../pages/home/home.component';
-import { RouteObject } from 'react-router-dom';
-
-export const GlobalRoute: RouteObject[] = [
-	{
-		path: '/',
-		element: <Home />,
-	},
-];
